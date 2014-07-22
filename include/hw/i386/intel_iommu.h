09a44c847a975c0a23c1facb9482ada7ee0167b2
Le Tan <tamlokveer@gmail.com>
2014-07-22 15:58:09 +0800

    intel-iommu: modify the copyright information
    Modify the copyright information of intel_iommu.c and intel_iommu.h.
    
    Signed-off-by: Le Tan <tamlokveer@gmail.com>

--- include/hw/i386/intel_iommu.h
+++ include/hw/i386/intel_iommu.h
@@ -1,26 +1,28 @@
 /*
- * QEMU emulation of an Intel IOMMU
+ * QEMU emulation of an Intel IOMMU (VT-d)
  *   (DMA Remapping device)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * Copyright (c) 2013 Knut Omang, Oracle <knut.omang@oracle.com>
+ * Copyright (C) 2014 Le Tan, <tamlokveer@gmail.com>
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * Lots of defines copied from kernel/include/linux/intel-iommu.h:
  *   Copyright (C) 2006-2008 Intel Corporation
  *   Author: Ashok Raj <ashok.raj@intel.com>
  *   Author: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
- *
- * Copyright (c) 2013 Knut Omang, Oracle <knut.omang@oracle.com>
  *
  */
 
