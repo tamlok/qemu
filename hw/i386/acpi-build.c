tamlok/qemu

commit 4c80a4110b4356a1f7e882937a15fcb5a9f9ec7b

acpi: use VTD_HOST_ADDRESS_WIDTH instead of magic number
Use VTD_HOST_ADDRESS_WIDTH to build dmar table.

Signed-off-by: Le Tan <tamlokveer@gmail.com>

hw/i386/acpi-build.c
@@ -1325,7 +1325,7 @@ build_dmar_q35(GArray *table_data, GArray *linker)
     AcpiDmarHardwareUnit *drhd;
 
     dmar = acpi_data_push(table_data, sizeof(*dmar));
-    dmar->host_address_width = 0x26;    /* 0x26 + 1 = 39 */
+    dmar->host_address_width = VTD_HOST_ADDRESS_WIDTH;
     dmar->flags = 0;    /* No intr_remap for now */
 
     /* DMAR Remapping Hardware Unit Definition structure */

commit 328376ca956c22bc28d189a0cbad8c951a8e2783

acpi: fix host address width in DMAR table
The haw field of DMAR table should be the real host address width minus 1.
This fixes: 4c80a4110b4356a1f7e882937a15fcb5a9f9ec7b

Signed-off-by: Le Tan <tamlokveer@gmail.com>

hw/i386/acpi-build.c
@@ -1325,7 +1325,7 @@ build_dmar_q35(GArray *table_data, GArray *linker)
     AcpiDmarHardwareUnit *drhd;
 
     dmar = acpi_data_push(table_data, sizeof(*dmar));
-    dmar->host_address_width = VTD_HOST_ADDRESS_WIDTH;
+    dmar->host_address_width = VTD_HOST_ADDRESS_WIDTH - 1;
     dmar->flags = 0;    /* No intr_remap for now */
 
     /* DMAR Remapping Hardware Unit Definition structure */